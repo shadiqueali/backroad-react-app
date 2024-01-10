ec16dac9f66069ab6f72e8a0d3ada81a515b1ad8
shadiqueali <sadikali322@gmail.com>
2024-01-10 17:56:27 +0530

    first commmit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Abouts from "./components/Abouts";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+
+
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+    <Hero/>
+    <Abouts/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+
+    
+    
+
+   
+
+    
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Tours.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Title from './Title'
+import { tours } from '../data';
+
+const Tours = () => {
+  return (
+    <section className="section" id="tours">
+      <Title title="featured" subTitle="tours" />
+
+      <div className="section-center featured-center">
+        {tours.map((tour)=>{
+          const {id,images,data,title,info,location,duration,cost} = tour
+          return(
+            <article className="tour-card" key={id}>
+            <div className="tour-img-container">
+              <img src={images} className="tour-img" alt="" />
+              <p className="tour-date">{data}</p>
+            </div>
+            <div className="tour-info">
+              <div className="tour-title">
+                <h4>{title}</h4>
+              </div>
+              <p>
+              { info}
+              </p>
+              <div className="tour-footer">
+                <p>
+                  <span>
+                    <i className="fas fa-map"></i>
+                  </span>{" "}
+                  {location}
+                </p>
+                <p>{duration} days</p>
+                <p>from ${cost}</p>
+              </div>
+            </div>
+          </article>
+          )
+        })}
+       
+
+        
+          
+      </div>
+    </section>
+  );
+}
+
+export default Tours
